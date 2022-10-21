6f6173290f9573a915dc0a4e3f2484613e5d007a
Wenbo Pan <wenbopan313@gmail.com>
2022-10-21 00:29:11 -0700

    a6 React.js sp22

--- src/App.js
+++ src/App.js
@@ -9,32 +9,31 @@ import {Routes, Route} from "react-router";
 
 function App() {
   return (
-      <BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
+        <BrowserRouter>
           <div className="container">
-	          <Routes>
-		          <Route index element={<Labs/>}/>
-		          <Route path="/hello" element={<HelloWorld/>}/>
-		          <Route path="/tuiter/*" element={<Tuiter/>}/>
-	          </Routes>
+            <Routes>
+              <Route index element={<Labs/>}/>
+              <Route path="/hello" element={<HelloWorld/>}/>
+              <Route path="/tuiter/*" element={<Tuiter/>}/>
+            </Routes>
           </div>
-      </BrowserRouter>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+        </BrowserRouter>
+      </header>
+    </div>
   );
 }
 
