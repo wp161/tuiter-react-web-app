0698398ca549d43a92baf6215e9eb283fe02e56d
Wenbo Pan <wenbopan313@gmail.com>
2022-11-02 21:42:23 -0700

    update before a7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
-
-import {BrowserRouter} from "react-router-dom";
-import {Routes, Route} from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
