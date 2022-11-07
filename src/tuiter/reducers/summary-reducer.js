23b072e2f21277351512b92a272431b3e6dd3d7c
Wenbo Pan <wenbopan313@gmail.com>
2022-11-06 17:07:55 -0800

    a7 Redux fa22

--- src/tuiter/reducers/summary-reducer.js
+++
@@ -1,9 +0,0 @@
-// import posts from "../data/posts.json"
-// import {createSlice} from "@reduxjs/toolkit";
-//
-// const postsSlice = createSlice({
-// 	name: 'posts',
-// 	initialState: posts
-// })
-//
-// export default postsSlice.reducer
