f97534693e2a0e659c830e8da7b3c34f62cb9548
Wenbo Pan <wenbopan313@gmail.com>
2022-11-12 23:36:45 -0800

    updated paths

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // const TUITS_API = 'https://tuiter-node-app-server.herokuapp.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
+console.log(TUITS_API)
 
 export const createTuit = async (tuit) => {
 	const response = await axios.post(TUITS_API, tuit)
